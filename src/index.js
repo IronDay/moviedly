5ac2ba6973836a3b5ecb8abe1acb9c776b7097a7
Florian <nsiegningflorian@gmail.com>
2024-06-24 18:10:59 +0200

    Update index.js to inject the PORT dynamically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,6 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log("Listening on port 3000...");
 });
