ef84b1ddbe53429ce2c47dfe12d4dc0406560ec1
Djioleu Florian <nsiegningflorian@gmail.com>
2024-06-24 19:07:01 +0200

    Add some variable to test the env variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -10,4 +11,11 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Listening on port ${PORT}...`);
+    if (app.get('env') === 'production') {
+        console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    }
+
+    if(app.get('env') === 'development') {
+        console.log('AP_PASSWORD: ', process.env.APP_PASSWORD);
+    }
 });
