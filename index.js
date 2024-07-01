9f25f2ef075af97621c5c6ce19f1df73c3199fdb
Florian <nsiegningflorian@gmail.com>
2024-07-01 13:50:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log("Mongo URL ", MONGO_URL);
 
 const app = express();
 
-mongoose.connect(`${MONGO_URL} || "mongodb://localhost:27017/moviedly"}`)
+mongoose.connect(`${MONGO_URL}`)
     .then(() => {
         console.log("Connected to MongoDB...");
     })
