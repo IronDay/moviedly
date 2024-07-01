dae565ad59705b0f8fa5e36f06b76277644b7b30
Djioleu Florian <nsiegningflorian@gmail.com>
2024-07-01 17:22:21 +0200

    Add the route to save a customer

--- routes/customer.js
+++ routes/customer.js
@@ -15,6 +15,17 @@ const customerRouter = express.Router();
 customerRouter.get("/", async (req, res) => {
     const customers = await Customer.find({}).sort({name: 1});
     res.status(200).send(customers);
+});
+
+customerRouter.post("/",async (req,res)=>{
+    const customer = req.body;
+    const {error} = validateCustomer(customer);
+
+    if(error) return res.status(400).send(error.message);
+    const c = new Customer(customer);
+    const savedCustomer = await c.save();
+
+    return res.status(201).send(savedCustomer);
 })
 
 const validateCustomer = (customer) => {
