fd9b5e790247019f775693bba17628e1f02203d3
Djioleu Florian <nsiegningflorian@gmail.com>
2024-07-02 21:35:41 +0200

    Add the Movie model

---
+++ models/movies.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import {Genre} from "./genres.js";
+import Joi from "joi";
+
+const MovieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50,
+        trim: true,
+    },
+    genre: {type: Genre.schema, required: true},
+    numberInStock: {type: Number, required: true, min: 0, max: 255},
+    dailyRentalRate: {type: Number, min: 0, max: 255}
+});
+
+const Movie = mongoose.model("Movies", MovieSchema);
+
+function validateMovies(movie) {
+    const schema = {
+        title: Joi.string().min(5).max(50).required(),
+        genre: Joi.string().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: Joi.number().min(0).required()
+    }
+
+    return Joi.validate(movie, schema);
+}
+
+export {Movie, validateMovies as validate}
