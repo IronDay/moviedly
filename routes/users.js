78d3d6790efd9845fa37e3b2e3d95c8f560fa2fc
Djioleu Florian <nsiegningflorian@gmail.com>
2024-07-10 22:24:06 +0200

    Add the route to create an user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { Users } from "../models/users.js";
+import { Users, validate } from "../models/users.js";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", (req, res) => {
   return res.status(200).send(Users.find());
+});
+
+usersRouter.post("/", (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) return res.send(400).send(error.details[0].message);
+
+  const user = new Users({ ...req.body }).save();
+  return res.send(user);
 });
