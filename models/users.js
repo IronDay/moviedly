5295b9ac3b11f16d99e15d65460c2a5b3b586e60
Djioleu Florian <nsiegningflorian@gmail.com>
2024-07-13 01:22:32 +0200

    refactor the user schema to include a method to generate the token

--- models/users.js
+++ models/users.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 import Joi from "joi";
+import jwt from "jsonwebtoken";
+import config from "config";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 5, maxLength: 20 },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+};
 
 const UsersModel = mongoose.model("Users", userSchema);
 
