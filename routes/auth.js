e9d5f714e388267c28fd6c73ec2861620d46f197
Djioleu Florian <nsiegningflorian@gmail.com>
2024-07-12 23:34:55 +0200

    refactor the auth route to use the asynchronous compare func from bcrypt

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { Users } from "../models/users.js";
+import bcrypt from "bcrypt";
+import Joi from "joi";
+
+const authRoute = express.Router();
+
+authRoute.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let fetchedUser = await Users.findOne({ email: req.body.email });
+  if (!fetchedUser) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    fetchedUser.password,
+  );
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  res.send(true);
+});
+
+function validate(req) {
+  const userSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  return userSchema.validate(req);
+}
+
+export default authRoute;
